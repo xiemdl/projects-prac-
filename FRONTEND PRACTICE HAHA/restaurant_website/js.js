35ee7f4d058bcdb142cd28447b50f8025f834295
Shiela Madel Domingo <shielamadelanonuevodomingo1804@gmail.com>
2025-09-01 12:58:03 +0800

    Add files via upload

---
+++ FRONTEND PRACTICE HAHA/restaurant_website/js.js
@@ -0,0 +1,22 @@
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextBtn = document.querySelector('.carousel-btn.next');
+const prevBtn = document.querySelector('.carousel-btn.prev');
+let currentIndex = 0;
+
+function updateCarousel() {
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateCarousel();
+});
+
+window.addEventListener('resize', updateCarousel);
---
+++ FRONTEND PRACTICE HAHA/try.js
@@ -0,0 +1,8 @@
+console.log("jjajaja");
+ var a = 1;
+ var b = 2;
+
+function add(x, y) {
+    console.log("Adding numbers:", x, y);
+    return x + y;
+}
